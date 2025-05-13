4b519ab5924f81cd7a55975303867e84181679ce
saiiswarya1998 <saiiswarya1998@gmail.com>
2025-05-14 00:26:12 +0530

    second commit

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -33,7 +33,7 @@ const sendEmail = async (req, res) => {
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
   const info = await sgMail.send(msg);
-  res.json(info);
+  res.status(200).json({"msg":"success"})
 };
 
 module.exports = sendEmail;
